c7acc7142c3dd4d8c9accb7fd98bfd3869b7a3a5
sszeb <jack.z.k.davies@gmail.com>
2014-12-15 11:47:36 +1300

    finshed alpha design, started work on sign-up.html

--- MyLiftingPal/js/main.js
+++ MyLiftingPal/js/main.js
@@ -1,10 +1,21 @@
+//Toggling ful view of exercises with
 function slideToggle(number){
     $(".tabsdiv"+number).slideToggle(400);
 }
 
-
-function myFunction(){
+function toggleTest(){
     slideToggle(1);
     slideToggle(2);
     slideToggle(3);
 }
+//////////////////////////////////
+document.forms["signup"].submit();
+
+function submitform()
+{
+    var wanted_value = form.someFieldName.value; 
+
+    console.log(wanted_value);
+ 
+}
+
